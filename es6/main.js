222ec478ce8c4bda5d06cd1863494aa56161f2ca
Grace Gamble <grace.h.gamble@gmail.com>
2018-12-24 16:17:51 -0500

    Let and Const

---
+++ es6/main.js
@@ -0,0 +1,20 @@
+// ES5
+/*var myName = 'Grace';
+console.log(myName);
+
+myName = 'Eric';
+console.log(myName);*/
+
+//ES6 - Let, can change value
+/*let myName = 'Grace';
+console.log(myName);
+
+myName = 'Eric';
+console.log(myName);*/
+
+//ES6 - Const, will receive error because can't rename value
+const myName = 'Grace';
+console.log(myName);
+
+myName = 'Eric';
+console.log(myName);
---
+++ es6/readme.md
@@ -0,0 +1,7 @@
+###Var
+
+#Let is new Var
+Use for variable values,
+
+#Const
+Constant values, never change, const will never receive a new value
